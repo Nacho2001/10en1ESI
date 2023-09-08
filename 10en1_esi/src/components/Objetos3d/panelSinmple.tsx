971edafb87cb32249fc02fc966a716e2c0f8ff6b
Nacho2001 <nachomigoni@gmail.com>
2023-09-08 15:29:49 -0300

    continuar con variables de objeto

--- 10en1_esi/src/components/Objetos3d/panelSinmple.tsx
+++ 10en1_esi/src/components/Objetos3d/panelSinmple.tsx
@@ -1,10 +1,17 @@
 import React, {useEffect, useRef} from "react";
 import * as THREE from 'three';
+import { ObjectLoader } from "three";
+import './creadorObjetos';
 
 const PanelSimple = () => {
     const canvasRef = useRef();
     useEffect(() => {
-
+        const anchoCamera=window.innerWidth;
+        const largoCamera=window.innerHeight; 
+        let escenaArray = [];
+        escenaArray = escena(75,anchoCamera,largoCamera, 0.1, 1000);
+        let objetoArray = [];
+        objetoArray = objeto();
         animacion();
     }, [])
     return ( 
