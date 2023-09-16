890ba10696007294ba091f90686dc2c0bf71029d
Nacho <nachomigoni@gmail.com>
2023-09-16 10:34:08 -0300

    Inicio escena en tsx

---
+++ 10en1_esi/src/components/Objetos3d/Escena.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useRef } from 'react';
+import * as THREE from 'three';
+//import { OrbitControls } from 'three/addons/libs/three.module.js';
+
+export const Escena: React.FC = () => {
+    const canvasRef = useRef<HTMLCanvasElement | null>(null);
+    useEffect(() => {
+        if (!canvasRef.current) return;
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        const renderer = new THREE.WebGLRenderer({canvas: canvasRef.current});
+        renderer.setSize(window.innerWidth, window.innerHeight)
+
+        scene.background = new THREE.Color(0x0000ff)
+
+        const geometry = new THREE.BoxGeometry();
+        const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+        const cube = new THREE.Mesh(geometry, material);
+        scene.add(cube);
+
+        camera.position.z = 5
+        const animate = () => {
+            requestAnimationFrame(animate);
+      
+            // Rotación del cubo
+            if (cube) {
+                console.log("cube true")
+              cube.rotation.x += 0.01;
+              cube.rotation.y += 0.01;
+            }
+      
+            renderer.render(scene, camera);
+          };
+    }, [])
+    return (
+        <>
+        <canvas ref={canvasRef} style={{ width: '100%', height: '100%' }}></canvas>
+        </>
+    )
+}
--- 10en1_esi/src/pages/Home.tsx
+++ 10en1_esi/src/pages/Home.tsx
@@ -2,6 +2,7 @@ import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/rea
 import ExploreContainer from '../components/ExploreContainer';
 import PanelSimple from '../components/Objetos3d/panelSinmple';
 import './Home.css';
+import { Escena } from '../components/Objetos3d/Escena';
 
 const Home: React.FC = () => {
   return (
@@ -17,7 +18,9 @@ const Home: React.FC = () => {
             <IonTitle size="large">Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <div id="screen"></div>
+        <div id="screen">
+          <Escena/>
+        </div>
       </IonContent>
     </IonPage>
   );
