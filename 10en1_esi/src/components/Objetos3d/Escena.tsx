3c6d6e46921ca1efb01c2fb8e515f211ab6cdc94
Nacho2001 <nachomigoni@gmail.com>
2023-09-16 17:52:19 -0300

    escena añadida

--- 10en1_esi/src/components/Objetos3d/Escena.tsx
+++ 10en1_esi/src/components/Objetos3d/Escena.tsx
@@ -1,40 +1,41 @@
-import React, { useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import * as THREE from 'three';
-//import { OrbitControls } from 'three/addons/libs/three.module.js';
-// Ver @types/threea
+import fondo from './texturas/red-brick-wall-background-vintage-brickwork-texture-photo.jpg';
+
+// Funcion con la escena vacia
 export const Escena: React.FC = () => {
-    const canvasRef = useRef<HTMLCanvasElement | null>(null);
+    // 
+    const refCanvas = useRef<HTMLCanvasElement | null>(null);
+    // useEffect para declarar las constantes cuando se renderice el componente
     useEffect(() => {
-        if (!canvasRef.current) return;
-        const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera(75, window.innerWidth/2 / window.innerHeight/2, 0.1, 1000);
-        const renderer = new THREE.WebGLRenderer({canvas: canvasRef.current});
-        renderer.setSize(window.innerWidth/2, window.innerHeight/2)
+        // Si el elemento canva no esta activo, el componente no retorna nada
+        if (!refCanvas.current) return;
+        // Crea un instancia de la clase Scene de THREE 
+        const escena = new THREE.Scene();
+        // Crea una camara de perspectiva apuntando
+        const camara = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
+        // Define el renderizador y muestra la escena en el canvas
+        const renderizador = new THREE.WebGLRenderer({canvas: refCanvas.current});
+        // Establece resolucion de la escena
+        renderizador.setSize(window.innerWidth, window.innerHeight/2)
+        // importa los objetos creados en otros archivos
 
-        scene.background = new THREE.Color(0x0000ff)
 
-        const geometry = new THREE.BoxGeometry();
-        const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-        const cube = new THREE.Mesh(geometry, material);
-        scene.add(cube);
-
-        camera.position.z = 5
-        const animate = () => {
-            requestAnimationFrame(animate);
-      
-            // Rotación del cubo
-            if (cube) {
-                console.log("cube true")
-              cube.rotation.x += 0.01;
-              cube.rotation.y += 0.01;
-            }
-      
-            renderer.render(scene, camera);
-          };
+        // Crea una instancia del cargador de textura para importar la imagen de fondo
+        const cargadorTextura = new THREE.TextureLoader()
+        // importa la imagen con el cargador de textura
+        const imagenFondo = cargadorTextura.load(fondo)
+        // Define la imagen como fondo de la escena
+        escena.background = imagenFondo;
+        // Establece posición de la camara
+        camara.position.z = 5
+        // El renderizador genera la escena con la camara creada
+        renderizador.render(escena, camara);
     }, [])
+    // El componente retorna un elemento canvas, que contiene la escena completa
     return (
         <>
-        <canvas ref={canvasRef} style={{ width: '100%', height: '100%' }}></canvas>
+        <canvas ref={refCanvas} style={{ width: '100%', height: '100%' }}></canvas>
         </>
     )
 }
