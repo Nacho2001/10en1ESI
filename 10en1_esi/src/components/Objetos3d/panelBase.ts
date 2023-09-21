269a1f2a2ef3a77e9b6ee965bf8b7a435ecf14f1
Nacho2001 <nachomigoni@gmail.com>
2023-09-21 16:51:14 -0300

    crear escena con fondo transparente

--- 10en1_esi/src/components/Objetos3d/panelBase.ts
+++ 10en1_esi/src/components/Objetos3d/panelBase.ts
@@ -1,8 +1,11 @@
 import * as THREE from 'three';
-
-export function panel(x:number,y:number,z:number,color:string){
+import {createText} from 'three/examples/jsm/webxr/Text2D';
+export function panel(x:number,y:number,z:number,color:string,texto:string){
     const figura = new THREE.BoxGeometry(x,y,z);
-    const material = new THREE.MeshBasicMaterial({color: `#ff0000`})
+    const material = new THREE.MeshBasicMaterial({color: color})
     const panel = new THREE.Mesh(figura,material);
+    //const textoPanel = createText(texto,40)
+    //panel.add(textoPanel)
+    //textoPanel.position.set(0,0.06,0)
     return panel;
 }
